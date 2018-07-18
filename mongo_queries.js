7d08077b4e3acabf30e76c81ea98e35d93e33d85
Eugene Tawiah <eugene.tawiah@gmail.com>
2018-07-17 23:50:08 -0400

    YMGFTH-2 - adding some cleanup queries, cheat sheet for next time

---
+++ mongo_queries.js
@@ -0,0 +1,25 @@
+db.getCollection('AAPL').find({"open" : 189.75})
+db.getCollection('DPW').find({"date" : 1531849287})
+db.getCollection('posts').find({})
+db.printCollectionStats()
+
+
+
+
+// ##### this is dangerous territory #####
+db.getCollection('AAPL').drop()
+db.getCollection('ABIL').drop()
+db.getCollection('AVXL').drop()
+db.getCollection('BLNK').drop()
+db.getCollection('CRON').drop()
+db.getCollection('DEST').drop()
+db.getCollection('DPW').drop()
+db.getCollection('EGY').drop()
+db.getCollection('GEVO').drop()
+db.getCollection('JVA').drop()
+db.getCollection('KBLB').drop()
+db.getCollection('NAKD').drop()
+db.getCollection('POTN').drop()
+db.getCollection('RKDA').drop()
+db.getCollection('SNES').drop()
+// ##### this is dangerous territory #####
